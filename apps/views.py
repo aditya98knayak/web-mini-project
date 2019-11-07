3cda7162d5dcd826cf42435d7d51689fe2690d38
adityaknayak <adik.nayak@gmail.com>
2019-11-07 22:02:05 +0530

    views updated

--- apps/views.py
+++ apps/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request,'initial.html')
+def login(request):
+    return render(request,'dashboard.html')
